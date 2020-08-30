0b74539a1633712eb1f18d50b645be3df260b839
arvishcdoshi <doshi.arvish16@gmail.com>
2020-08-30 15:16:31 +0530

    Minor fixes

--- app/core/models.py
+++ app/core/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, \
                                        
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,password=None,**extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
             raise ValueError("Users must have an email address")
-        user = self.model(email=self.normalize_email(email),**extra_fields)  #for creating user
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         
