29e04b48e1129f19366d181ad1fdb56986d8d219
arvishcdoshi <doshi.arvish16@gmail.com>
2020-09-27 18:01:59 +0530

    Issue in manage user endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -5,6 +5,9 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    #path('create/users/<int:pk>/', views.CreateUserView.as_view()),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+    
 ]
 
--- app/user/views.py
+++ app/user/views.py
@@ -1,15 +1,33 @@
 from user.serializers import UserSerializer, AuthTokenSerializer
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+    #http_method_names = ['get', 'post']
+    #queryset = get_user_model().objects.all()
+
+    # def get(self, request, *args, **kwargs):
+    #     users = get_user_model().objects.all()
+    #     serializer = AuthTokenSerializer(users, many=True)
+    #     return Response(serializer.data)
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication)
+    permission_classes = (permissions.IsAuthenticated)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
 
