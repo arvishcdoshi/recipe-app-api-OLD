92b2bfef0f59c0353e9140536216e6ef50fdcde5
arvishcdoshi <doshi.arvish16@gmail.com>
2021-01-01 13:51:20 +0530

    Changes for integrating Pillow Package

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,4 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static' # static files will be dumped here when the project is built
+
+#127.0.0.1:8000/static or media  --> to access
+
 AUTH_USER_MODEL = 'core.User'
